67e961acdcbb1ef6c999c901964aec6b5171b243
vmorte <virginia.morte@gmail.com>
2015-04-23 16:19:09 +0200

    animal provisional

--- target/classes/static/app/routeconfig.js
+++ target/classes/static/app/routeconfig.js
@@ -7,5 +7,5 @@ function RouteConfig($routeProvider) {
 	$routeProvider.when('/', { templateUrl : "templates/inicio.html" });
 	$routeProvider.when('/newpost', { templateUrl : "templates/newpost.html" });
 	$routeProvider.when('/post/:id', { templateUrl : "templates/post.html" });
-
+	$routeProvider.when('/adminAnimales', { templateUrl : "templates/adminAnimales.html" });
 }
