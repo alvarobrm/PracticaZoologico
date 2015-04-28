454490892f9fa3e37bdc9d3ab7cbdd89c2555e68
vmorte <virginia.morte@gmail.com>
2015-04-28 16:50:46 +0200

    He quitado lo que no hacia falta

--- src/main/resources/static/app/routeconfig.js
+++ src/main/resources/static/app/routeconfig.js
@@ -4,10 +4,6 @@ RouteConfig.$inject = [ '$routeProvider' ];
 
 function RouteConfig($routeProvider) {
 
-	$routeProvider.when('/animallist', { templateUrl : "templates/postlist.html" });
-	$routeProvider.when('/newpost', { templateUrl : "templates/newpost.html" });
-	$routeProvider.when('/post/:id', { templateUrl : "templates/post.html" });
-	
 	$routeProvider.when('/', { templateUrl : "templates/animallist.html" });
 	$routeProvider.when('/newanimal', { templateUrl : "templates/newanimal.html" });
 	$routeProvider.when('/animal/:id', { templateUrl : "templates/animal.html" });
